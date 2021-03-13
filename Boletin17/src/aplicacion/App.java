389a3c5c40ce0a71a3a706fc7ff7049c47380622
Sergio Counago Dominguez <scounagodominguez@danielcastelao.org>
2021-03-13 11:38:33 +0100

    Boltin 17 correccion

--- Boletin17/src/aplicacion/App.java
+++ Boletin17/src/aplicacion/App.java
@@ -8,10 +8,11 @@ import javax.swing.*;
 public class App {
     public static void main(String[] args) {
         int op=1;
+        Libreria.crearEstante();
         while(op!=0){
             op = Integer.parseInt(JOptionPane.showInputDialog("Introducir 0 para saír," +
                     "1 para engadir libro, 2 para vender libro, 3 para amosar os libros, 4 para dar de baixa libros con 0 uds," +
-                    "5 para consultar un libro"));
+                    "5 para consultar un libro, 6 para crear un novo estante"));
             switch (op){
                 case 1: Libreria.engadirLibro(); break;
                 case 2: Libreria.venderLibro(JOptionPane.showInputDialog("Introducir título ou ISBN"));break;
